131b9692cc6ddc0476c8af5717db13b96eb9a67c
JuanJesusRamirez <jramirez@flar.net>
2025-03-11 12:08:28 -0500

    Update workflows

--- terraform/import-prd.sh
+++ terraform/import-prd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the subscription ID from environment variable or use the default
+SUBSCRIPTION_ID=${TF_VAR_subscription_id:-"960eb1ea-f872-46cb-bdee-af0c8c00607c"}
+
 # Import the existing Container App into Terraform state for production environment
 echo "Importing existing Container App into Terraform state..."
-terraform import azurerm_container_app.app /subscriptions/960eb1ea-f872-46cb-bdee-af0c8c00607c/resourceGroups/RG_RALFPRD/providers/Microsoft.App/containerApps/caralfprd
+terraform import azurerm_container_app.app /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/RG_RALFPRD/providers/Microsoft.App/containerApps/caralfprd
--- terraform/import.sh
+++ terraform/import.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the subscription ID from environment variable or use the default
+SUBSCRIPTION_ID=${TF_VAR_subscription_id:-"960eb1ea-f872-46cb-bdee-af0c8c00607c"}
+
 # Import the existing Container App into Terraform state
 echo "Importing existing Container App into Terraform state..."
-terraform import azurerm_container_app.app /subscriptions/960eb1ea-f872-46cb-bdee-af0c8c00607c/resourceGroups/RG_RALFV2/providers/Microsoft.App/containerApps/caralfuatv2
+terraform import azurerm_container_app.app /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/RG_RALFV2/providers/Microsoft.App/containerApps/caralfuatv2
