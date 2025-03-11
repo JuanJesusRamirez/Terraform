ccdf2227987505cd769fdef573067c898145c5de
JuanJesusRamirez <jramirez@flar.net>
2025-03-10 20:31:08 -0500

    Create terraform account

---
+++ scripts/setup_backend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set variables
+RESOURCE_GROUP_NAME="terraform-state-rg"
+STORAGE_ACCOUNT_NAME="tfstateralfr3v2"
+CONTAINER_NAME="tfstate"
+LOCATION="eastus"
+
+# Create resource group
+echo "Creating resource group $RESOURCE_GROUP_NAME..."
+az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+
+# Create storage account
+echo "Creating storage account $STORAGE_ACCOUNT_NAME..."
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+
+# Create blob container
+echo "Creating blob container $CONTAINER_NAME..."
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --resource-group $RESOURCE_GROUP_NAME
