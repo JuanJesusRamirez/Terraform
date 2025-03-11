13791e15d26c721884f3f34182ca857b8957f89e
JuanJesusRamirez <jramirez@flar.net>
2025-03-10 21:00:22 -0500

    Save account

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   
   backend "azurerm" {
-    resource_group_name  = "terraform-state-ralfv2-rg"
+    resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstateralfv2"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
